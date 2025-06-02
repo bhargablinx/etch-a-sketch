35aae01fb2c7bc1411a6b748d104adc8800a0447
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-02 07:53:53 +0000

    border color changed

--- src/script.js
+++ src/script.js
@@ -1,13 +1,26 @@
 const gridContainer = document.querySelector(".main-grid-cont");
 let gridCount = 16;
+renderGrid();
 
-for (let i = 0; i < gridCount; i++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("border", "flex-1", "flex");
-    for (let j = 0; j < gridCount; j++) {
-        const colDiv = document.createElement("div");
-        colDiv.classList.add("border", "border-b-0", "flex-1", "flex");
-        rowDiv.appendChild(colDiv);
+function renderGrid() {
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < gridCount; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("border", "flex-1", "flex");
+        for (let j = 0; j < gridCount; j++) {
+            const colDiv = document.createElement("div");
+            colDiv.classList.add("border", "border-b-0", "flex-1", "flex");
+            rowDiv.appendChild(colDiv);
+        }
+        gridContainer.appendChild(rowDiv);
     }
-    gridContainer.appendChild(rowDiv);
 }
+
+document.querySelector(".grid-change-btn").addEventListener("click", () => {
+    gridCount = prompt("Number of Grids: (3-100)");
+    if (gridCount < 3 || gridCount > 100) {
+        alert("Invalid Grid Size!!");
+    } else {
+        renderGrid();
+    }
+});
